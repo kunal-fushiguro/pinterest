f491373ab809c2a5944ae18028b8dc152a9244db
kunal-fushiguro <natwarlaluzumaki@gmail.com>
2025-09-22 11:17:16 +0530

    restart

--- src/components/LoadingCricle.tsx
+++
@@ -1,9 +0,0 @@
-const LoadingCircle = () => {
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-12 h-12 border-5 border-gray-300 border-t-red-600 rounded-full animate-spin" />
-    </div>
-  );
-};
-
-export default LoadingCircle;
--- src/lib/appwrite.ts
+++
@@ -1,8 +0,0 @@
-import { Client, Account, Databases } from "appwrite";
-
-export const client = new Client()
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
-
-export const account = new Account(client);
-export const databases = new Databases(client);
